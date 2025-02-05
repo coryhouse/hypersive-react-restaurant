7603316c4f6747e6618596c261e426678a3d28f1
Cory House <housecor@gmail.com>
2025-02-05 14:34:35 -0600

    add

--- app/admin-rsc/page.tsx
+++ app/admin-rsc/page.tsx
@@ -1,17 +1,33 @@
-import { Food } from "./food";
-import type { Food as FoodType } from "../food";
+import { FoodItem } from "@/app/admin-rsc/food-item";
+import { Food } from "@/app/food";
+import { Spinner } from "@/components/spinner";
+import { Suspense } from "react";
 
-export default async function Page() {
-  const resp = await fetch("http://localhost:3001/fods");
-  if (!resp.ok) throw new Error("Failed to fetch foods");
-  const foods = (await resp.json()) as FoodType[];
+async function FoodList() {
+  const resp = await fetch("http://localhost:3001/foods");
+
+  if (!resp.ok) {
+    throw new Error("Failed to fetch foods");
+  }
+
+  const foods = await resp.json();
 
   return (
-    <>
-      <h1>Admin Page</h1>
-      {foods.map((food) => (
-        <Food key={food.id} food={food} />
+    <div className="flex flex-wrap gap-6">
+      {foods.map((food: Food) => (
+        <FoodItem key={food.id} food={food} />
       ))}
+    </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <>
+      <h1>Admin</h1>
+      <Suspense fallback={<Spinner />}>
+        <FoodList />
+      </Suspense>
     </>
   );
 }
